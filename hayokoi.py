b29aa366aa2fffc6a6380a3402fe1e077fc12c35
Akkey <120071886+Akkey735@users.noreply.github.com>
2023-01-17 15:59:44 +0900

    Update hayokoi.py

--- hayokoi.py
+++ hayokoi.py
@@ -1,1 +1,10 @@
-print("🐵来い" * 500)
+import threading
+
+def super_monkey_face():
+  print("🐵来い" * 500)
+
+def main():
+  threading.Thread(target=super_monkey_face).start()
+
+if __name__ == "__main__":
+  main()
